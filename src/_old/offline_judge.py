3544ac30a513eea81c22dae5262a44ec009975f1
Ivan Torlakov <itorlakov@gmail.com>
2023-07-03 13:27:16 +0300

    Some changes

---
+++ src/_old/offline_judge.py
@@ -0,0 +1,30 @@
+import subprocess
+
+
+class OfflineJudge:
+  def __init__(self):
+    self.program_path = None
+
+  def run_test_case(self, expected_result):
+    try:
+      process = subprocess.Popen(
+        ["python", self.program_path],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        universal_newlines=True
+      )
+      output, error = process.communicate(timeout=5)
+
+      if process.returncode != 0:
+        return False, f"Runtime Error:\n{error}"
+
+      output = output.strip()
+      if output == str(expected_result):
+        return True, "Correct Answer"
+      else:
+        return False, f"Wrong Answer:\nExpected Result: {expected_result}\nActual Result: {output}"
+
+    except subprocess.TimeoutExpired:
+      return False, "Time Limit Exceeded"
+
+    except Exception as e:
+      return False, f"Error occurred: {str(e)}"
--- src/main.py
+++ src/main.py
@@ -4,59 +4,66 @@ import subprocess
 import json
 
 class OfflineJudge:
-    def __init__(self):
-        self.program_source = None
+  def __init__(self):
+    self.program_source = None
 
-    def run_test_case(self, expected_result):
-        try:
-            with open("temp_program.py", "w") as file:
-                file.write(self.program_source)
+  def run_test_case(self, expected_result):
+    response = {
+      result: False,
+      expected = expected_result
+    }
 
+    # add data for memory/cpu consuption, execution time
+    try:
+      with open("temp_program.py", "w") as file:
+        file.write(self.program_source)
 
-            process = subprocess.Popen(
-                ["python", "temp_program.py"],
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                universal_newlines=True
-            )
-            output, error = process.communicate(timeout=5)  # Timeout set to 5 seconds
+      process = subprocess.Popen(
+        ["python", "temp_program.py"],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        universal_newlines=True
+      )
+      output, error = process.communicate(timeout=5)  # Timeout set to 5 seconds
 
+      if process.returncode != 0:
+        response.strerr = "Runtime error"
+        response.error = error
+        return response
 
-            if process.returncode != 0:
-                return False, f"Runtime Error:\n{error}"
+      output = output.strip()
+      response.output = output
+      if output == str(expected_result):
+        response.result = True
 
-            output = output.strip()
-            if output == str(expected_result):
-                return True, "Correct Answer"
-            else:
-                return False, f"Wrong Answer:\nExpected Result: {expected_result}\nActual Result: {output}"
+    except subprocess.TimeoutExpired:
+      response.strerr = "Time Limit Exceeded"
 
-        except subprocess.TimeoutExpired:
-            return False, "Time Limit Exceeded"
+    except Exception as e:
+      response.error = str(e)
 
-        except Exception as e:
-            return False, f"Error occurred: {str(e)}"
+    # delete temp file temp_program.py
+    return response
 
 async def start_websocket_server(websocket, path):
-    offline_judge = OfflineJudge()
+  offline_judge = OfflineJudge()
 
-    async for message in websocket:
-        data = json.loads(message)
-        program_source = data.get('source')
-        expected_result = data.get('expected')
+  async for message in websocket:
+    data = json.loads(message)
+    exec_type = data.get('type')
+    program_source = data.get('source')
+    expected_result = data.get('expected')
 
-        offline_judge.program_source = program_source
-        result, message = offline_judge.run_test_case(expected_result)
+    if exec_type == 'source':
+      offline_judge.program_source = program_source
+      response = offline_judge.run_test_case(expected_result)
+    elif exec_type == 'file':
+      # write for file logic + upload to server
 
-        response = {
-            "result": result,
-            "message": message
-        }
-
-        await websocket.send(json.dumps(response))
+    await websocket.send(json.dumps(response))
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(
-        websockets.serve(start_websocket_server, "localhost", 8765)
-    )
-    asyncio.get_event_loop().run_forever()
+  asyncio.get_event_loop().run_until_complete(
+    websockets.serve(start_websocket_server, "localhost", 8765)
+  )
+  asyncio.get_event_loop().run_forever()
 
